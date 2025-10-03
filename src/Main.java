3d0e86ca361876218e4ab7b0a32366c37317ce06
vlad-bol <Kiborg_S-3@mail.ru>
2025-10-03 14:54:05 +0300

    import static

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,53 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
+import static java.lang.Math.*;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+
+class Lab1_combo {
+    void main() {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1-Переменные 2-Математические функции 3-Логические ветвления 0-Выход");
+            switch (sc.nextLine()) {
+                case "1":
+                    System.out.print("Введите x,y: ");
+                    double x = sc.nextDouble(), y = sc.nextDouble();
+                    sc.nextLine();
+                    double min = min(x, y);
+                    System.out.println("Меньшее число после деления на10: " + (min/10));
+                    break;
+
+                case "2":
+                    System.out.print("Введите a, b: ");
+                    double a = sc.nextDouble(), b = sc.nextDouble();
+                    sc.nextLine();
+                    double maxAB = max(a, b);
+                    double minAB = min(a, b);
+                    System.out.println("Наибольшее max(a,b): " + maxAB);
+                    System.out.println("Наименьшее min(a,b): " + minAB);
+
+                    double num = 5* max(a, b + 2) - 4 * min(1 - a, b);
+                    double den = 3 + max(a, b +2) * min(1 - a, b);
+                    System.out.println("Результат: " + (den != 0 ? num/den : "Ошибка"));
+                    break;
+
+                case "3":
+                    System.out.print("Введите x: ");
+                    double x2 = sc.nextDouble();
+                    sc.nextLine();
+                    double Y = x2 <= -2 ? pow(x2, 5)- tan(2 * x2 - 1) :
+                            x2 <= 1 ? 3 * x2 * (1+ exp(x2 + 1)) : pow(sin(x2), 5 );
+                    double F = x2 <= -2 ? exp(sin(x2)) :
+                            x2 <= 1 ? x2 * x2 : cos(x2) >= 0 ? sqrt(cos(x2)) : Double.NaN;
+                    System.out.println("Y=" + Y + " F=" + F);
+                    break;
+
+                case "0":
+                    sc.close();
+                    return;
+
+                default: System.out.println("Ошибка!");
+            }
+        }
+    }
 }
